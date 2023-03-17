84dd2d1933a7fb28b0928c44af11d13b464d1676
Alvin.wang <alvin.wang@wayz.ai>
2023-03-17 11:32:08 +0800

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,31 @@
+// vite.config.js
+import { readdirSync } from 'fs'
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import commonjs from 'vite-plugin-commonjs'
+
+function getInput(dir) {
+  return readdirSync(resolve(__dirname, dir)).reduce((pages, page) => {
+    pages[page] = resolve(__dirname, `${dir}/${page}/index.html`)
+    return pages
+  }, {})
+}
+
+export default defineConfig({
+  assetsInclude: ['**/*.gltf', '**/*.glb'],
+  resolve: {
+    alias: {
+      '~': resolve(__dirname, './'),
+      '@': resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        ...getInput('fundamentals'),
+      },
+    },
+  },
+  plugins: [commonjs()],
+})
